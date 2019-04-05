f31274dc2e7500e44aef7f9c9798dfb845969d9e
Heather Zurek <heather.zurek@gmail.com>
2019-04-05 11:23:16 -0700

    make the code work

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,20 @@
 function beepBoop(numberInput) {
  var numberOut = [];
 
- for (var i = 0; i <= numberInput; i++) {
-   if (i.toString().includes("1")){
-     numberOut.push(" Beep!");
-   } else if (i.toString().includes("2")) {
-       numberOut.push(" Boop!");
-   } else if (i % 3 === 0) {
-       numberOut.push(" I'm sorry, Dave. I'm afraid I can't do that.")
-     } else {
-       numberOut.push(" " + i);
-
-       }
+for (var i=0; i<=numberInput; i++) {
+      if (i.toString().includes("1")) {
+        numberOut.push(" Beep!");
+      } else if (i.toString().includes("2")) {
+        numberOut.push(" Boop!");
+      } else if (i.toString().includes("3")) {
+        numberOut.push(" I'm sorry Dave. I'm afraid I can't do that!");
+      }else {
+        numberOut.push(' ' + i);
       }
-    return numberOut;
     }
+    return numberOut;
+   }
+
 
 //front end logic
 $(document).ready(function() {
