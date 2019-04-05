606c1610cc8d0b2f9c4411fa7ea1b715557a4f43
Heather Zurek <heather.zurek@gmail.com>
2019-04-05 15:10:00 -0700

    reverse order of else if statements and finish dry code

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-//back end logic
+//business logic
 function beepBoop(numberInput) {
- var numberOut = [];
+  var numberOut = [];
 
-for (var i=0; i<=numberInput; i++) {
-      if (i.toString().includes("32")) {
-        numberOut.push(" I'm sorry, Dave. I'm afraid I can't do that."); //The number 13 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
-      } else if (i.toString().includes("21")) {
-        numberOut.push(" Boop"); //The number 21 should be replaced with "Boop".
-      } else if (i.toString().includes("13")) {
-        numberOut.push(" I'm sorry, Dave. I'm afraid I can't do that."); //The number 32 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
-      } else if (i.toString().includes("1")) {
-        numberOut.push(" Beep!");
-      } else if (i.toString().includes("2")) {
-        numberOut.push(" Boop!");
-      } else if (i.toString().includes("3")) {
-        numberOut.push(" I'm sorry Dave. I'm afraid I can't do that.");
-      } else {
-        numberOut.push(' ' + i);
-      }
+  for (var i=0; i<=numberInput; i++) {
+    if (i.toString().includes("32")) {
+      numberOut.push(" I'm sorry, Dave. I'm afraid I can't do that."); //The number 32 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+    } else if (i.toString().includes("3")) {
+      numberOut.push(" I'm sorry Dave. I'm afraid I can't do that.");
+    } else if (i.toString().includes("21")) {
+      numberOut.push(" Boop"); //The number 21 should be replaced with "Boop".
+    } else if (i.toString().includes("2")) {
+      numberOut.push(" Boop!");
+    } else if (i.toString().includes("13")) {
+      numberOut.push(" I'm sorry, Dave. I'm afraid I can't do that."); //The number 13 should be replaced with "I'm sorry, Dave. I'm afraid I can't do that."
+    } else if (i.toString().includes("1")) {
+      numberOut.push(" Beep!");
+    } else {
+      numberOut.push(' ' + i);
     }
-    return numberOut;
-   }
+  }
+  return numberOut;
+}
 
-//front end logic
+//user interface logic
 $(document).ready(function() {
   $("form#beep").submit(function(event) {
     event.preventDefault();
