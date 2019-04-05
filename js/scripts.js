1f6c8e81a2e9f414993751d45a4b70dced109141
Heather Zurek <heather.zurek@gmail.com>
2019-04-05 09:43:44 -0700

    print beep, boop, and im sorry dave

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,45 @@
-//back end logic
-// function beepBoop(numberInput) {
-//   var numbersOut = "";
-//
-//   for (var i = numberInput; i > 0; i-=1) {
-//     if (numberInput >= 1){
-//       numbersOut += "I";
-//
-//   // for (var i = numberInput; i < 20; i++) {
-//   //   if (numberInput >= 20 ){
-//   //     numbersOut += "Hi";
-//   //     console.log(numbersOut);
-//     }
-//   }
-//   return numbersOut;
-// }
-var beepBoop = function(numberInput) {
-  return true;
-};
+// //back end logic
+function beepBoop(numberInput) {
+ var numberOut = "";
 
 
-//front end logic
+   // for (var i = numberInput; i > 0; i-=0) {
+   //   if (numberInput >= 9){
+   //     numberOut += "HELPP MEEEEE";
+   //     numberInput -= 0;
+   //     }
+   //   }
+     for (var i = numberInput; i > 0; i-=3) {
+       if (numberInput >= 3){
+         numberOut += "I'm sorry, Dave. I'm afraid I can't do that.";
+         numberInput -= 3;
+         }
+       }
+ for (var i = numberInput; i > 0; i-=2) {
+   if (numberInput >= 2){
+     numberOut += "Boop!";
+     numberInput -= 2;
+     }
+   }
+   for (var i = numberInput; i > 0; i-=1) {
+     if (numberInput >= 1){
+       numberOut += "Beep!";
+     } else {
+
+     }
+    }
+   return numberOut;
+ }
+
+//working logic
+// var beepBoop = function(numberInput) {
+//   return true;
+// };
+
 $(document).ready(function() {
-  $("form#beep-boop").submit(function(event) {
+  $("form#beep").submit(function(event) {
     event.preventDefault();
-    var numberInput = parseInt($("input#numberEnrty").val());
+    var numberInput = parseInt($("input#numbersIn").val());
     var output = beepBoop(numberInput);
 
     $("#result").text(output);
